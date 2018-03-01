055bdd5baf9f6192fb7843d949d6766782fc2415
Carlos Menezes <carlos.shaka02@gmail.com>
2018-03-01 18:25:45 -0300

    Add miner's reward

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,5 +1,6 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
+const { MINING_REWARD } = require('../config');
 
 describe('transaction', () => {
   let transactions, wallet, recipient, amount;
@@ -64,4 +65,15 @@ describe('transaction', () => {
         .toEqual(nextAmount);
     });
   });
+
+  describe('creating a reward transaction', () => {
+    beforeEach(() => {
+      transaction = Transaction.rewardTransaction(wallet, Wallet.blockchainWallet());
+    });
+
+    it(`reward the miner's wallet`, () => {
+      expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+        .toEqual(MINING_REWARD);
+    });
+  })
 })
