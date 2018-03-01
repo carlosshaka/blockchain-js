38137cbbef2dcd31648c45a7251d70f8f157d6fd
Carlos Menezes <carlos.shaka02@gmail.com>
2018-03-01 15:53:20 -0300

    Add transaction pool

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -23,6 +27,10 @@ app.post('/mine', (req, res) => {
 
   res.redirect('/blocks');
 });
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
+})
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
